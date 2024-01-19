c51bf3beb0b2431e79ca1a8b39811cf08cc8d4bb
sanadajv1 <sanadajv@student.apc.edu.ph>
2024-01-19 16:31:46 +0800

    Update Products.blade.php

--- resources/views/YearnArt/Products.blade.php
+++ resources/views/YearnArt/Products.blade.php
@@ -34,7 +34,7 @@
                      <div class="option_container">
                         <div class="options">
                            <a href="" class="option1">
-                           Men's Shirt
+                           Product Details
                            </a>
                            <a href="" class="option2">
                            Buy Now
